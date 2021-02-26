80a317b6c388514499ea24dfb83d8465c50c55fd
Muhammad Rabiul Alam <bm.rabiulalam@gmail.com>
2021-02-26 17:54:38 +0600

    Simple React SPA - Done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total, product) => total + product.price, 0);
+
+    let shipping = 0;
+    if(total > 35){
+        shipping = 0;
+    }
+    else if(total > 15){
+        shipping = 4.99;
+    }
+    else if(total > 0){
+        shipping = 12.99;
+    }
+
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2)
+
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
+    return (
+        <div>
+            <h3>This is a cart</h3>
+            <h5>Product Summary: {cart.length}</h5>
+            <p>Product Price: {formatNumber(total)}</p>
+            <p><small>Shipping Cost: {shipping}</small></p>
+            <p><small>Tax + VAT: {tax}</small></p>
+            <p>Total Price: {grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
